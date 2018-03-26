1db57c506a3dd50bdc67731785d436a1b46ca7e6
Oli Desborough <oli.desborough@ovoenergy.com>
2018-03-26 17:21:15 +0100

    Updated install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-ln -sfv ~/dotfiles/.Xresources ~
-ln -sfv ~/dotfiles/.spacemacs ~
-ln -sfv ~/dotfiles/.xinitrc ~
-ln -sfv ~/dotfiles/.xprofile ~
+ln -sfv ~/.dotfiles/.Xresources ~
+ln -sfv ~/.dotfiles/.spacemacs ~
+ln -sfv ~/.dotfiles/.xinitrc ~
+ln -sfv ~/.dotfiles/.xprofile ~
 
-ln -sfv ~/dotfiles/.config/bspwm/  ~/.config
-ln -sfv ~/dotfiles/.config/sxhkd/  ~/.config
-ln -sfv ~/dotfiles/.config/polybar/  ~/.config
+ln -sfv ~/.dotfiles/.config/bspwm/  ~/.config
+ln -sfv ~/.dotfiles/.config/sxhkd/  ~/.config
+ln -sfv ~/.dotfiles/.config/polybar/  ~/.config
